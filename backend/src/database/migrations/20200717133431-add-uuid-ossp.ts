02374ce23e7311966362a01aef694babe7eb964d
jjluizgomes27 <jjluizgomes27@gmail.com>
2026-06-26 14:48:04 -0300

    Make uuid-ossp migration safe for MySQL

--- backend/src/database/migrations/20200717133431-add-uuid-ossp.ts
+++ backend/src/database/migrations/20200717133431-add-uuid-ossp.ts
@@ -1,10 +1,15 @@
-import { QueryInterface, DataTypes, Sequelize } from "sequelize";
+import { QueryInterface } from "sequelize";
 
 module.exports = {
-  up: (queryInterface: QueryInterface) => {
-    return Promise.all([
-      queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'),
-    ]);
+  up: async (queryInterface: QueryInterface) => {
+    if (queryInterface.sequelize.getDialect() === "postgres") {
+      await queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    }
   },
 
+  down: async (queryInterface: QueryInterface) => {
+    if (queryInterface.sequelize.getDialect() === "postgres") {
+      await queryInterface.sequelize.query('DROP EXTENSION IF EXISTS "uuid-ossp"');
+    }
+  }
 };
