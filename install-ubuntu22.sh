b1e7b271708a9ab59ca44730336319adfe5387cd
jjluizgomes27 <jjluizgomes27@gmail.com>
2026-06-26 14:53:14 -0300

    Add installer placeholder

---
+++ install-ubuntu22.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Instalador Whaticket 2025 em preparação."
+echo "Repo: https://github.com/jjluizgomes27/Whaticket-2025"
+echo "Frontend: app.kryolo.com.br"
+echo "API: api.kryolo.com.br"
